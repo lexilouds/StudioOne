cfcf2a7e529564a2a3342271d6a7db5d968b9e94
lexilouds <lexiloudiadis@gmail.com>
2025-01-22 11:38:04 -0800

    Refactored Input Handling

---
+++ Assets/BallController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody sphereRigidBody;
+    [SerializeField] float ballSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    public void MoveBall(Vector2 input)
+    {
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        sphereRigidBody.AddForce(inputXZPlane * ballSpeed);
+    }
+
+    void Update()
+    {
+       
+    }
+}
